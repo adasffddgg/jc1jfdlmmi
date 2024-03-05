a233b39c4db755e709fe0d1a085aa00fd475c8ce
Adas Adas <57170768+adasffddgg@users.noreply.github.com>
2024-03-05 14:20:45 +0530

    initial commit

---
+++ src/main/java/io/github/adasffddgg/Calculator.java
@@ -0,0 +1,11 @@
+package io.github.adasffddgg;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
